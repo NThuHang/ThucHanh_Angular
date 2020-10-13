82f647d2202a95d12798229cc5884a16532edb45
NThuHang <nguyenthuhang11121999@gmail.com>
2020-10-14 01:15:05 +0700

    TH02

--- TH02/src/app/app.module.ts
+++ TH02/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Bai1Component } from './bai1/bai1.component';
+import { HeaderComponent } from './bai1/header/header.component';
+import { BodyComponent } from './bai1/body/body.component';
+import { FooterComponent } from './bai1/footer/footer.component';
+import { Bai2Component } from './bai2/bai2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Bai1Component,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    Bai2Component
   ],
   imports: [
     BrowserModule,
